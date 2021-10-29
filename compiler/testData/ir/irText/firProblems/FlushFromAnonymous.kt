27d4aa973663d26e80f80c221b84c8e8586230c3
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-10-29 16:27:19 +0300

    FIR2IR: create local classes on the fly properly

---
+++ compiler/testData/ir/irText/firProblems/FlushFromAnonymous.kt
@@ -0,0 +1,20 @@
+// FILE: Collector.java
+
+public class Collector {
+    public void flush() {}
+}
+
+// FILE: FlushFromAnonymous.kt
+
+class Serializer() {
+    fun serialize() {
+        val messageCollector = createMessageCollector()
+        try {
+
+        } catch (e: Throwable) {
+            messageCollector.flush()
+        }
+    }
+
+    private fun createMessageCollector() = object : Collector() {}
+}
