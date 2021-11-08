53dfa81d39d4d66e240bc789eaf928ac56e706e6
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2021-11-08 14:40:35 +0300

    JVM_IR prefer loop with inclusive bound for unsigned integer ranges
    In general, we would rather prefer a range-based for loop to look like
    a counter loop in Java ('for (i = start; i < end; ++i) { <BODY> }').
    This corresponds to
    
        i = start;
        do {
            if (i >= end) break;
            <BODY>
        } while ( { ++i; true } )
    
    However, HotSpot doesn't recognize Kotlin unsigned integer comparison
    in 'if (i >= end) break;' as a counter loop condition. Thus, the loop
    doesn't get optimized as a counter loop, resulting in a performance
    regression.
    If we use exclusive range-based for loop instead, then we actually use
    unsigned integer equality instead of unsigned integer comparison, which
    is Ok for HotSpot.
    
    KT-49444

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/loops/handlers/DownToHandler.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/loops/handlers/DownToHandler.kt
@@ -63,7 +63,7 @@ internal class DownToHandler(private val context: CommonBackendContext) :
 
     private fun IrExpression.convertToExclusiveLowerBound(progressionType: ProgressionType): IrExpression? {
         if (progressionType is UnsignedProgressionType) {
-            if (this.constLongValue == 0L) return null
+            if (preferJavaLikeCounterLoop || this.constLongValue == 0L) return null
         }
 
         val irConst = this as? IrConst<*> ?: return null
